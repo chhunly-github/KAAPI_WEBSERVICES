5a9ccdb770830adc9908e50f14938b89af4ba4d1
User <User@192.168.178.27>
2016-08-11 11:12:01 +0700

    I update user playlist.....

--- src/main/java/org/khmeracademy/v3/services/impl/UserPlayListServiceImpl.java
+++ src/main/java/org/khmeracademy/v3/services/impl/UserPlayListServiceImpl.java
@@ -1,15 +1,18 @@
 package org.khmeracademy.v3.services.impl;
 
+import java.util.ArrayList;
+
 import org.khmeracademy.v3.entities.elearning.Playlist;
 import org.khmeracademy.v3.repository.user.UserPlayListRepository;
 import org.khmeracademy.v3.services.user.UserPlayListService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 @Service
 public class UserPlayListServiceImpl implements UserPlayListService{
-
+	@Autowired
 	private UserPlayListRepository userplaylistrepository;
 	@Override
-	public Playlist findPlaylistByUserId(int userid) {
+	public ArrayList<Playlist> findPlaylistByUserId(int userid) {
 		// TODO Auto-generated method stub
 		return userplaylistrepository.findPlaylistByUserId(userid);
 	}
